e883096802673831cec08b0bc40d55f43a76d766
capptn <info@capptn.com>
2020-11-28 17:34:19 +0100

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -15,10 +15,10 @@ then
 	rm master.zip
 
 	mkdir -p venus-liontron-Master/ext/velib_python
-	cp -R velib_python-master/* venus-liontronbms-Master/ext/velib_python
+	cp -R velib_python-master/* venus-liontron-Master/ext/velib_python
 
 	echo "Add Chargery entries to serial-starter"
-	sed -i  's/ENV{VE_SERVICE}="rs485:default"/ENV{VE_SERVICE}="rs485:default:chargerybms"/g' /etc/udev/rules.d/serial-starter.rules
+	echo 'ACTION=="add", ENV{ID_BUS}="usb", ENV{ID_MODEL}=="CP2102_USB_to_UART_Bridge_Controller",          ENV{VE_SERVICE}="liontronbms"' >> /etc/udev/rules.d/serial-starter.rules
 	sed -i  '/service.*imt.*dbus-imt-si-rs485tc/a service liontronbms     liontronbms' /etc/venus/serial-starter.conf
 
 	echo "Install Chargery driver"
