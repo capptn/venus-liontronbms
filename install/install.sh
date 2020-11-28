e6754af14fa56927181a0937cd1fe0b192319592
capptn <info@capptn.com>
2020-11-28 16:03:45 +0100

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -6,16 +6,16 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	echo "Download driver and library"
 
-	wget https://github.com/capptn/venus-liontronbms/archive/master.zip
-	unzip master.zip
-	rm master.zip
+	wget https://github.com/capptn/venus-liontronbms/archive/Master.zip
+	unzip Master.zip
+	rm Master.zip
 
 	wget https://github.com/victronenergy/velib_python/archive/master.zip
 	unzip master.zip
 	rm master.zip
 
-	mkdir -p venus-liontron-master/ext/velib_python
-	cp -R velib_python-master/* venus-liontronbms-master/ext/velib_python
+	mkdir -p venus-liontron-Master/ext/velib_python
+	cp -R velib_python-master/* venus-liontronbms-Master/ext/velib_python
 
 	echo "Add Chargery entries to serial-starter"
 	echo 'ACTION=="add", ENV{VE_SERVICE}="rs485:default:liontronbms", ENV{ID_MODEL}=="CP2102_USB_to_UART_Bridge_Controller",          ENV{VE_SERVICE}="liontronbms"' >> /etc/udev/rules.d/serial-starter.rules
@@ -24,8 +24,8 @@ then
 	echo "Install Chargery driver"
 	mkdir -p /var/log/liontronbms
 	mkdir -p /opt/victronenergy/liontronbms
-	cp -R venus-liontronbms-master/ext /opt/victronenergy/liontronbms
-	cp -R venus-liontronbms-master/driver/* /opt/victronenergy/liontronbms
+	cp -R venus-liontronbms-Master/ext /opt/victronenergy/liontronbms
+	cp -R venus-liontronbms-Master/driver/* /opt/victronenergy/liontronbms
 
 	chmod +x /opt/victronenergy/liontronbms/start-liontronbms.sh
 	chmod +x /opt/victronenergy/liontronbms/liontronbms.py
@@ -36,18 +36,18 @@ then
 
 	echo "Copy gui files"
 
-	cp venus-liontronbms-master/gui/qml/MbItemRowTOBO.qml /opt/victronenergy/gui/qml
-	cp venus-liontronbms-master/gui/qml/MbTextDescriptionTOBO.qml /opt/victronenergy/gui/qml
-	cp venus-liontronbms-master/gui/qml/PageBatteryChargeryBMS.qml /opt/victronenergy/gui/qml
-	cp venus-liontronbms-master/gui/qml/PageBatteryChargeryBMSVoltages.qml /opt/victronenergy/gui/qml
-	cp venus-liontronbms-master/gui/qml/PageMain.qml /opt/victronenergy/gui/qml
+	cp venus-liontronbms-Master/gui/qml/MbItemRowTOBO.qml /opt/victronenergy/gui/qml
+	cp venus-liontronbms-Master/gui/qml/MbTextDescriptionTOBO.qml /opt/victronenergy/gui/qml
+	cp venus-liontronbms-Master/gui/qml/PageBatteryChargeryBMS.qml /opt/victronenergy/gui/qml
+	cp venus-liontronbms-Master/gui/qml/PageBatteryChargeryBMSVoltages.qml /opt/victronenergy/gui/qml
+	cp venus-liontronbms-Master/gui/qml/PageMain.qml /opt/victronenergy/gui/qml
 
 	read -p "Setup new gui overview? [Y to proceed]" -n 1 -r
 	echo    # (optional) move to a new line
 	if [[ $REPLY =~ ^[Yy]$ ]]
 	then
 		echo "Setup new overview"
-		cp venus-liontronbms-master/gui/qml/OverviewTiles.qml /opt/victronenergy/gui/qml
+		cp venus-liontronbms-Master/gui/qml/OverviewTiles.qml /opt/victronenergy/gui/qml
 	fi
 
 	echo "To finish, reboot the Venus OS device"
